7c29b6e51f0c5ec26240c2006de72bc9083c8e64
User <user@jarreds-mbp.mynet>
2021-06-01 01:07:04 +0100

    add exception

--- dottygen/generator/channels.py
+++ dottygen/generator/channels.py
@@ -71,7 +71,7 @@ class OutChannel(ChannelInstance):
                 indentation)
         if self._send_err:
             function_writer.write_line(
-                f'if(false){{throw RuntimeException("Some exception")}}',
+                f'if(false){{throw Exception("Some exception")}}',
                 indentation)
         if isWebsite:
             function_writer.write_line(f'send({first_char_lower(self.get_channel_name())},{self._labels[0].get_payload_assign_string()}) >> {{',
